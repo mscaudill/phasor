0df659e27ce0d76968ffd0aa7fccb5986d091e2a
mscaudill <mscaudill@gmail.com>
2024-01-17 15:34:28 -0600

    [dev]: islices method to generate 1D arrays from an ndarray

--- src/phasor/core/numerical.py
+++ src/phasor/core/numerical.py
@@ -1,7 +1,10 @@
 """
 """
 
+import itertools
+
 import numpy as np
+import numpy.typing as npt
 import scipy.signal as sps
 
 def analytic(data, axis=-1, N=None):
@@ -74,10 +77,66 @@ def phases(analytic, deg=True):
     return x
 
 
+def normalize_axis(axis: int, ndim: int):
+    """Returns a positive axis index for a supplied axis index of an ndim
+    array.
+
+    Args:
+        axis:
+            An positive or negative integer axis index.
+        ndim:
+            The number of dimensions to normalize axis by.
+    """
+
+    axes = np.arange(ndim)
+    return axes[axis]
+
+
+def islices(x: npt.NDArray, axis: int = -1):
+    """An iterator yielding successive 1D slices from an ndarray.
+
+    Args:
+        x:
+            An ndarray from which 1D slices will be made.
+        axis:
+            The axis of x containing the elements of each 1D slice.
+
+    Examples:
+        >>> x = np.arange(60).reshape(2, 3, 10)
+        >>> print(x)
+        >>> # elements along last axis will be elements in each slice
+        >>> items = islice(x, axis=-1)
+        >>> next(items)
+        array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        >>> next(items)
+        array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+        >>> next(items)
+        array([20, 21, 22, 23, 24, 25, 26, 27, 28, 29])
+
+    Yields:
+        Successive 1-D slices whose elements match x's elements along axis for
+        every 1D slice. The ordering of yieled arrays matches 'C' order in which
+        the first axis changes slowest.
+    """
+
+    ax = normalize_axis(axis, x.ndim)
+
+    idxs = [range(s) for axis, s in enumerate(x.shape) if axis != ax]
+    for multiindex in itertools.product(*idxs):
+
+        slicer = [slice(idx, idx+1) for idx in multiindex]
+        slicer.insert(ax, slice(None))
+
+        yield np.squeeze(x[tuple(slicer)])
+
+
+
+
 if __name__ == '__main__':
 
     import time
 
+    """
     #x = np.random.random((40, 3, 500 * 90))
     amplitude = 2
     freq = 8
@@ -94,4 +153,8 @@ if __name__ == '__main__':
     print(f'Elapsed = {time.perf_counter() - t0} s')
 
     p = phases(sa)
+    """
+
+    x = np.random.randint(0, 17, size=(4,50))
+    b = list(iter_slices(x))
 
