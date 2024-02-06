5eb09b700b0caa679dc31f7a80bb8ec6c6c4c41d
mscaudill <mscaudill@gmail.com>
2024-02-06 17:08:39 -0600

    [dev]: adding ambiguity function but still must resolve how to return frequencies because the fft returns them differently with zero on left which is not helpful for plotting, FFTShift takes care of this but will complicate further transforms.

--- src/phasor/core/arraytools.py
+++ src/phasor/core/arraytools.py
@@ -1,5 +1,7 @@
 """A collection of tools for working with numpy ndarrays."""
 
+from typing import Sequence
+
 import numpy as np
 import numpy.typing as npt
 
@@ -54,4 +56,89 @@ def islices(x: npt.NDArray, axis: int = -1):
         slicer.insert(ax, slice(None))
 
         yield np.squeeze(x[tuple(slicer)])
+
+def pad_along_axis(
+    arr: npt.NDArray, pad: Sequence, axis: int = -1, **kwargs
+) -> npt.NDArray:
+    """Wrapper for numpy pad allowing before and after padding along
+    a single axis.
+
+    Args:
+        arr (ndarray):              ndarray to pad
+        pad (int or array-like):    number of pads to apply before the 0th
+                                    and after the last index of array along
+                                    axis. If int, pad number of pads will be
+                                    added to both
+        axis (int):                 axis of arr along which to apply pad.
+                                    Default pads along last axis.
+        **kwargs:                   any valid kwarg for np.pad
+    """
+
+    # convert int pad to seq. of pads & place along axis of pads
+    pad = [pad, pad] if isinstance(pad, int) else pad
+    pads = [(0, 0)] * arr.ndim
+    pads[axis] = pad
+    return np.pad(arr, pads, **kwargs)
+
+def pad_axis_to(
+    arr: npt.NDArray,
+    amt: int,
+    side: str,
+    axis: int = -1,
+    **kwargs,
+) -> npt.NDArray:
+    """Wrapper for numpy pad that allows for padding upto an integer length
+    along an axis.
+
+    Args:
+        arr:
+            A ndarray to pad along axis.
+        amt:
+            The desired length of arr along axis after padding.
+        side:
+            The side of array along axis which to pad. Must be one of 'left',
+            'right' or 'center'. 'left' pads the left side of axis of arr to
+            achieve amt length result, 'right' pads the right side of arr to
+            achieve amt length result and center pads the left and right by
+            equal amounts to achieve amt length result.
+        axis:
+            The axis along which the padding will be carried out.
+        kwargs:
+            Any valid kwarg for numpy's pad function.
+
+    Examples:
+        >>> x = np.ones((3, 4), dtype=int)
+        >>> pad_axis_to(x, 2, side='right')
+        array([[1, 1, 1, 1, 0, 0],
+               [1, 1, 1, 1, 0, 0],
+               [1, 1, 1, 1, 0, 0]])
+        >>> pad_axis_to(x, 1, side='left')
+        array([[0, 1, 1, 1, 1],
+               [0, 1, 1, 1, 1],
+               [0, 1, 1, 1, 1]])
+        >>> # uneven center padding places unpaired pad on right
+        >>> pad_along_axis(x, 3, side='center')
+        array([[0, 1, 1, 1, 1, 0, 0],
+               [0, 1, 1, 1, 1, 0, 0],
+               [0, 1, 1, 1, 1, 0, 0]])
+
+    Returns:
+        An ndarray whose shape matches arr on all axes except axis is length is
+        extended to amt.
+
+    Raises:
+        A ValueError is issued if side is not in {'left', 'right', 'center'}.
+    """
+
+    sides = {'left', 'right', 'center'}
+    if side.lower() not in sides:
+        raise ValueError(f'side must be one of {sides}')
+
+    pad_amt = amt - arr.shape[axis]
+    pads = {
+        'left': [pad_amt, 0],
+        'right': [0, pad_amt],
+        'center': [pad_amt // 2, pad_amt // 2 + pad_amt % 2],
+    }
+    return pad_along_axis(arr, pads[side.lower()], axis=axis, **kwargs)
 
--- src/phasor/distributuions.py
+++ src/phasor/distributuions.py
@@ -3,27 +3,121 @@ import numpy as np
 import scipy.signal as sps
 from scipy.linalg import hankel, toeplitz
 
-def ambiguity(signal, fs, analytic=True, axis=-1):
+from phasor.core.arraytools import pad_axis_to
+
+def wigner(signal, fs, analytic=True, axis=-1):
     """ """
 
     # I want to get this to work for ndarrays
-
-
-    # FIXME there are unknown issues here as the plots are not expected for
-    # ambiguity
-
     if analytic:
         x = sps.hilbert(signal, axis=axis)
     else:
         x = signal
 
-    hank = hankel(x)
-    print(hank)
-    y = np.roll(x, shift=1, axis=0)
-    lowtope = np.tril(toeplitz(y), -1)
+    max_shift = signal.shape[axis] // 2
+    
+    column0 = np.pad(x[max_shift::-1], (0, max_shift))
+    row0 = np.pad(x[max_shift:], (0, max_shift))
+    left_shifts = toeplitz(column0, row0)
+    right_shifts = np.flip(left_shifts, axis=0)
 
-    #FIXME I'm not sure what needs conjugating here and order!
-    return np.fft.ifft(np.conjugate(hank) * lowtope, axis=-1)
+    """
+    print(left_shifts, '\n')
+    print(right_shifts, '\n')
+    print('product ', left_shifts * right_shifts)
+    """
+
+    return np.fft.fft(np.conjugate(left_shifts) * right_shifts, axis=0)
+
+
+def ambiguity(data, fs, analytic=True):
+    """Returns the ambiguity of a 1-D data array.
+
+    The ambiguity function is the short-time Fourier transform of a signal with
+    a delayed copy of itself. It provides the distortion of a signal in terms of
+    this delay and a doppler frequency shift. Time frequency distributions are
+    built by mapping the signal to the ambiguity (delay, frequency shift) plane
+    where the response is filtered before Fourier transforming back to time and
+    frequency space.
+
+    Args:
+        data:
+            The data whose ambiguity is to be measured.
+        fs:
+            The sampling rate of the data array.
+        analytic:
+            A boolean indicating if data should be Hilbert transformed prior to
+            computing the ambiguity. This transfrom removes the negative
+            frequencies to prevent aliasing. Default is True.
+
+    Returns:
+        A 3-tuple consisting of (1) a len(data) x len(data) ambiguity  matrix
+        with doppler frequency shifts along the x-axis and doppler delays along
+        the y-axis, (2) a 1-D array of len(data) doppler frequencies in [0,
+        fs/2] and (3) a 1-D array of len(data) doppler lags in [-len(data),
+        len(data)].
+    Notes:
+        We compute the ambiguity response by constructing Toeplitz matrices that
+        hold both the forward and reverse data copies. Consider this signal [7,4,6].
+        Its delay can range from -3 to +3 here are the reversed & forward delayed
+        signals.
+
+         delay  Reversed  Forward  Product
+        -3      [0,0,0]   [0,0,0]  [0,0,0]
+        -2      [6,0,0]   [0,0,7]  [0,0,0]
+        -1      [4,6,0]   [0,7,4]  [0,42,0]
+         0      [7,4,6]   [7,4,6]  [49,16,36]
+         1      [0,7,4]   [4,6,0]  [0,42,0]
+         2      [0,0,7]   [6,0,0]  [0,0,0]
+         3      [0,0,0]   [0,0,0]  [0,0,0]
+
+        Notice that only where delay <= len(signal) // 2 = {-1,0,1} do we have
+        a non-zero product and that "reversed" is a Toeplitz and "forward" is
+        a flip of reversed on 0th axis.
+
+    References:
+        Najmi, A. H. "The Wigner distribution: A time-frequency analysis tool."
+        Johns Hopkins APL Technical Digest 15 (1994): 298-298.
+    """
+
+    x = np.array(data)
+    if x.ndim != 1:
+        msg = 'data must be exactly 1-D.'
+        raise ValueError(msg)
+
+    if analytic:
+        x = sps.hilbert(x)
+
+    # Construct Toeplitz
+    max_shift = len(x) // 2 if len(x) % 2 else len(x) // 2 - 1
+    first_col = pad_axis_to(x[max_shift::-1], 2 * max_shift + 1, side='right')
+    first_row = pad_axis_to(x[max_shift:], len(x), side='right')
+    reverse = toeplitz(first_col, first_row)
+    forward = np.flip(reverse, axis=0)
+
+    # FIXME we need to resolve the uncertainty about whether the amb is computed
+    # at negative frequencies. I think it must be
+
+    # compute ambiguity
+    amb = np.fft.ifft(np.conjugate(reverse) * forward, axis=1)
+    freqs = np.fft.fftfreq(len(x), d=1/fs)
+
+    # fftshift is useful for visualizing the autoterm but need not be returned
+    amb = np.fft.ifftshift(amb, axes=-1)
+    freqs = np.fft.ifftshift(freqs)
+
+    # FIXME we know b/c analytic there are no negative frequencies
+    """
+    amb = np.fft.ifftshift(amb, axes=-1)
+    freqs = np.fft.ifftshift(freqs)
+    idxs = slice(len(freqs)//2, None)
+    amb = amb[:, idxs]
+    freqs = freqs[idxs]
+    """
+
+    duration = len(x) / fs
+    lags = 2*np.arange(-max_shift, max_shift+1) / fs
+    return amb, lags, freqs
 
 
 
@@ -32,6 +126,7 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from phasor.data.synthetic import PACSignal
 
+    """
     duration = 10
     fs = 32
     time = np.linspace(0, duration, duration*fs+1)
@@ -44,5 +139,33 @@ if __name__ == '__main__':
     res = ambiguity(signal, fs=fs, analytic=True)
 
     fig, ax = plt.subplots()
-    ax.pcolormesh(np.linspace(0, fs/2, duration*fs+1), time, np.abs(res))
+    lags = np.linspace(-len(signal)/fs*2, len(signal)*fs*2, len(signal))
+    print(lags)
+    ax.pcolormesh(np.abs(res))
     plt.show()
+    """
+
+    """
+    signal = np.array([7,4,6,3, 2])
+    ambiguity(signal, fs=100, analytic=False)
+    """
+
+    
+    fs = 32
+    duration = 12
+    time = np.linspace(0, duration, duration*fs + 1)
+    #time = np.arange(0, 5, 1/fs)
+    #signal = np.sin(2*np.pi*4.5*time) + np.sin(2*np.pi*9*time)
+    signal = np.sin(2*np.pi*12*time)
+    signal[:6*fs] = 0
+    signal[10*fs:] = 0
+    #signal += np.random.random(fs*duration+1) * 0.25
+    #wig = wigner(signal, fs=fs)
+    amb, lags, freqs = ambiguity(signal, fs)
+
+    fig, ax = plt.subplots()
+    #ax.pcolormesh(time, freqs, np.abs(wig))
+    ax.pcolormesh(freqs, lags, np.abs(amb), shading='nearest')
+    #ax.pcolormesh(np.abs(amb), shading='flat')
+    plt.show()
+
